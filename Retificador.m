06767fb4d0a1c342c6fab6a9a00c67ba191bc60d
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-02-25 21:43:40 -0300

    Add files via upload

---
+++ Retificador.m
@@ -0,0 +1,29 @@
+%%%%%%%% limpeza %%%%%%%%%
+clear all
+close all
+clc
+format short eng
+
+%%%%% especificações do projeto %%%%%%
+
+Vrms = 220;
+f = 60;
+ripple = 0.5/100;
+Po = 80;
+
+%%%%% calculos  para retificador %%%%%
+Vmax = Vrms*sqrt(2);
+Vmin = Vmax*(1-ripple);
+Vo = (Vmax+Vmin)/2;
+Ro = (Vo^2)/Po;
+Iin = Po/Vrms;
+
+%%%%%%% Calculo do filtro %%%%%%%5
+C = (Po)/(f*((Vmax^2)-(Vmin^2)));
+
+%%%% DIODOS %%%%
+Vdmax = Vmax*1.5;
+Idmed = Vmax/(pi*Ro);
+
+
+
