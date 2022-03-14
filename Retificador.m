df2585c7628f9d30f8db14bcc48257be99b1d5b0
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-03-14 14:52:46 -0300

    Update Retificador.m

--- Retificador.m
+++ Retificador.m
@@ -7,14 +7,14 @@ format short eng
 %%%%% especificações do projeto %%%%%%
 
 Vrms = 220;                                #Tensão RMS
-f = 60;                                    # Frequencia em Hz
-ripple = 0.5/100;                          # Ripple de tensão
-Po = 80;                                   # Potencia de saida
+f = 60;                                    #Frequencia em Hz
+ripple = 0.5/100;                          #Ripple de tensão
+Po = 80;                                   #Potencia de saida
 
 %%%%% calculos  para retificador %%%%%
 Vmax = Vrms*sqrt(2);                      #Tensao maxima
 Vmin = Vmax*(1-ripple);                   #Tensao minima
-Vo = (Vmax+Vmin)/2;
+Vo = (Vmax+Vmin)/2;                       #Tensão de saida
 Ro = (Vo^2)/Po;
 Iin = Po/Vrms;
 
